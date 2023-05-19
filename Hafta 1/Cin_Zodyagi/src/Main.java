39eeeb82668e3f3d7feed0549420020be31ba16d
sametelmaci1 <133387220+sametelmaci1@users.noreply.github.com>
2023-05-19 13:30:27 +0300

    Add files via upload

---
+++ Hafta 1/Cin_Zodyagi/src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz : ");
+        int D_yili = scan.nextInt();
+
+        // Kullanıcın Doğum Yılına Göre Modunun Alındığı Kısım
+        int mod;
+        mod = D_yili % 12;
+
+        switch (mod){
+            case 0:
+                System.out.println("Çin Zodyağı Burcunuz : Maymun");
+                break;
+            case 1:
+                System.out.println("Çin Zodyağı Burcunuz : Horoz");
+                break;
+            case 2:
+                System.out.println("Çin Zodyağı Burcunuz : Köpek");
+                break;
+            case 3:
+                System.out.println("Çin Zodyağı Burcunuz : Domuz");
+                break;
+            case 4:
+                System.out.println("Çin Zodyağı Burcunuz : Fare");
+                break;
+            case 5:
+                System.out.println("Çin Zodyağı Burcunuz : Öküz");
+                break;
+            case 6:
+                System.out.println("Çin Zodyağı Burcunuz : Kaplan");
+                break;
+            case 7:
+                System.out.println("Çin Zodyağı Burcunuz : Tavşan");
+                break;
+            case 8:
+                System.out.println("Çin Zodyağı Burcunuz : Ejderha");
+                break;
+            case 9:
+                System.out.println("Çin Zodyağı Burcunuz : Yılan");
+                break;
+            case 10:
+                System.out.println("Çin Zodyağı Burcunuz : At");
+                break;
+            case 11:
+                System.out.println("Çin Zodyağı Burcunuz : Koyun");
+                break;
+        }
+    }
+}
